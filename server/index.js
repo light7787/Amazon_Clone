8e3ebd14d452b27e4b00a859e2a68861ad257a90
light7787 <chinufreefire1233@gmail.com>
2024-04-08 17:12:26 +0530

    hg

--- server/index.js
+++ server/index.js
@@ -171,10 +171,13 @@ function verifyToken(req,res,next){
    
 
 }
-const PORT = "https://amazon-clone-back.vercel.app" || 5000
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
-
-
+// Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
