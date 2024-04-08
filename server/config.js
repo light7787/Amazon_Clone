1655faaaf0d87654a3f151b218078818dabab839
light7787 <chinufreefire1233@gmail.com>
2024-04-08 19:08:39 +0530

    yty

--- server/config.js
+++ server/config.js
@@ -1,2 +1,5 @@
+const MONGODBURI =  "mongodb+srv://chinmayab7787:778788@cluster0.gkfb1n9.mongodb.net/ecommers?retryWrites=true&w=majority&appName=Cluster0"
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/ecommers');
+// mongoose.connect('mongodb://localhost:27017/ecommers');
+mongoose.connect(MONGODBURI);
