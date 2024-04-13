c5e2a62e691a4a29daae0f2f7afd105794fb2096
light7787 <chinufreefire1233@gmail.com>
2024-04-14 00:10:30 +0530

    jj

--- client/src/Address.jsx
+++ client/src/Address.jsx
@@ -1,37 +1,54 @@
-import React, { useEffect } from 'react'
-import Header from './Header'
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
 import AddIcon from '@mui/icons-material/Add';
 import { NavLink } from 'react-router-dom';
 
-
 const Address = () => {
     const auth = localStorage.getItem("user");
-const user = JSON.parse(auth);
-const username = user.name;
-    const searchHandle = async (event) => {
-        let key = username;
-          let result = await fetch(`https://amazon-clone-back.vercel.app/address/${key}`);
-          result = await result.json();
-          console.log(result);
-         
-      }
-      useEffect(()=>{
-        searchHandle();
+    const user = JSON.parse(auth);
+    const username = user.name;
+    const [addresses, setAddresses] = useState([]);
 
-      },[])
-  return (
-    <div>
-        <Header/>
-        <div className=" mt-10 ml-10">
-        <div className='text-lg'>Your Account›Your Addresses</div>
-        <div className='text-3xl'>Your Address</div>
-        <NavLink to="/add-address">
-        <div className='w-1/4 h-[300px] border-dashed border-2 border-black mt-4 flex flex-col pt-[50px] pl-[70px] rounded-xl  '>
-          <img className='h-[125px] w-[125px] ml-5 opacity-15' src="https://www.pngall.com/wp-content/uploads/10/Plus-Symbol-Vector-PNG-Picture.png" alt="" />
-          <div className='font-bold text-3xl'>Add address</div></div></NavLink>
-          </div>
-    </div>
-  )
-}
+    useEffect(() => {
+        fetchAddresses();
+    }, []);
 
-export default Address
+    const fetchAddresses = async () => {
+        try {
+            let response = await fetch(`https://amazon-clone-back.vercel.app/address/user/${username}`);
+            let data = await response.json();
+            setAddresses(data);
+        } catch (error) {
+            console.error('Error fetching addresses:', error);
+        }
+    };
+
+    return (
+        <div>
+            <Header />
+            <div className="mt-10 ml-10">
+                <div className='text-lg'>Your Account › Your Addresses</div>
+                <div className='text-3xl'>Your Address</div>
+                <NavLink to="/add-address">
+                    <div className='w-1/4 h-[300px] border-dashed border-2 border-black mt-4 flex flex-col pt-[50px] pl-[70px] rounded-xl'>
+                        <img className='h-[125px] w-[125px] ml-5 opacity-15' src="https://www.pngall.com/wp-content/uploads/10/Plus-Symbol-Vector-PNG-Picture.png" alt="" />
+                        <div className='font-bold text-3xl'>Add address</div>
+                    </div>
+                </NavLink>
+                <div className="grid grid-cols-3 gap-4 mt-8">
+                    {addresses.map((address, index) => (
+                        <div key={index} className="bg-white border rounded-lg p-4">
+                            <p className="text-lg font-semibold">{address.name}</p>
+                            <p>{address.mobile}</p>
+                            <p>{address.location}, {address.area}</p>
+                            <p>{address.city}, {address.state} - {address.pin}</p>
+                            <p>{address.defaults ? "Default Address" : ""}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Address;
