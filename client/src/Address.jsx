71290b73b305c762b4c70fa9bed73c5dc9ec4941
light7787 <chinufreefire1233@gmail.com>
2024-04-14 00:28:04 +0530

    hh

--- client/src/Address.jsx
+++ client/src/Address.jsx
@@ -37,8 +37,6 @@ const Address = () => {
                 </NavLink>
                 <div className="grid grid-cols-3 gap-4 mt-8">
                     {addresses.map((address, index) => (
-                        // Check if the address belongs to the current user
-                        (address.username === username) && (
                             <div key={index} className="bg-white border rounded-lg p-4">
                                 <p className="text-lg font-semibold">{address.name}</p>
                                 <p>{address.mobile}</p>
@@ -46,7 +44,7 @@ const Address = () => {
                                 <p>{address.city}, {address.state} - {address.pin}</p>
                                 <p>{address.defaults ? "Default Address" : ""}</p>
                             </div>
-                        )
+                        
                     ))}
                 </div>
             </div>
